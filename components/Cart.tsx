f3f69ba5a9729b76faceb9cd11adae9de6f38cdf
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-17 20:16:11 +0500

    Enhance Cart component: add remove item functionality, improve product details display, and adjust layout for better responsiveness

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -13,11 +13,11 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { useCart } from "@/lib/CartContext";
 import { urlFor } from "@/sanity/lib/image";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, X } from "lucide-react";
 import Link from "next/link";
 
 const Cart = () => {
-  const { cart, clearCart, incrementQuantity, decrementQuantity } = useCart();
+  const { cart, clearCart, incrementQuantity, decrementQuantity, removeFromCart } = useCart();
 
   const totalPrice = (productId: string, productColor: string, productSize: string): number => {
     const product = cart.find((item) => item.id === productId && item.color === productColor && item.size === productSize);
@@ -39,7 +39,7 @@ const Cart = () => {
       <Table>
         <TableHeader className="hidden md:table-header-group">
           <TableRow>
-            <TableHead>Product</TableHead>
+            <TableHead className="pl-20">Product</TableHead>
             <TableHead>Quantity</TableHead>
             <TableHead>Total</TableHead>
           </TableRow>
@@ -47,16 +47,24 @@ const Cart = () => {
         <TableBody>
           {cart.map((item) => (
             <TableRow key={`${item.id}-${item.color}-${item.size}`}>
-              <TableCell className="flex gap-x-4">
+              <TableCell className="flex gap-x-4 items-center">
+                <Button 
+                onClick={() => removeFromCart(item)}
+                className="bg-transparent hover:bg-transparent hover:-rotate-90 active:rotate-90 transition-transform transform duration-300 text-black hidden md:block"
+                >
+                  <X />
+                </Button>
                 <Image
                   src={urlFor(item.image).url()}
                   alt=""
                   width={200}
                   height={200}
+                  className="w-[150px] h-[200px] md:w-[200px] "
                 />
                 <div className="flex flex-col gap-4 justify-center">
-                  <h2 className="text-3xl">{item.name}</h2>
-                  <p className="max-w-xl">{item.description}</p>
+                  <h2 className="md:text-3xl">{item.name}</h2>
+                  <p className="max-w-xl"><span className="mr-1">Color:</span> {item.color}</p>
+                  <p className="max-w-xl"><span className="mr-1">Size:</span> {item.size}</p>
                   <span>{item.price}</span>
                   <div className="flex md:hidden items-center bg-gray-200 w-fit">
                     <Button
@@ -100,15 +108,7 @@ const Cart = () => {
           ))}
         </TableBody>
       </Table>
-      <div className="flex justify-between items-center mb-10 border border-x-0 border-b-0 px-4 py-4">
-        <div>
-          <Button
-            className="rounded-none bg-gray-200 hover:bg-gray-300 text-black active:scale-95 transition-transform transform duration-300"
-            onClick={clearCart}
-          >
-            Clear Cart
-          </Button>
-        </div>
+      <div className="flex justify-center items-end mb-10 border border-x-0 border-b-0 px-4 py-4">
         <div className="flex flex-col items-end justify-end space-y-4 w-full text-right">
           <div className="w-full flex justify-end items-center gap-4 px-4">
             <span>Subtotal:</span>
