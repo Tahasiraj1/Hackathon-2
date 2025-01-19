2416ad84dc02aae978d328963c8a5f23d673821c
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-19 21:21:30 +0500

    Done with product search functionlaity, added search component on header.

---
+++ components/SearchProduct.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Image as SanityImage } from "@sanity/types";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+
+interface Product {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  images: SanityImage;
+  ratings: string;
+  sizes: string[];
+  colors: string[];
+  tags: string[];
+  description: string;
+}
+
+const SearchProduct = () => {
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+  const [query, setQuery] = useState<string>("");
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.data))
+      .catch((error) => {
+        console.error("Error fetching featured products:", error);
+      });
+  }, []);
+
+  const getProductsName = (name: string) => {
+    const searchedProduct = products.find(
+      (p) => p.name.toLowerCase() === name.toLowerCase()
+    );
+    if (searchedProduct) {
+      router.push(`/products/${searchedProduct.id}`);
+      setShowSearch(false);
+      setQuery("");
+    }
+  };
+
+  const handleSearchClick = () => {
+    if (showSearch && query) {
+      getProductsName(query);
+    } else {
+      setShowSearch((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    if (showSearch && searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }, [showSearch]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        searchInputRef.current &&
+        !searchInputRef.current.contains(event.target as Node)
+      ) {
+        setShowSearch(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="relative flex items-center">
+      {showSearch && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            getProductsName(query);
+          }}
+          className="absolute animate-in slide-in-from-bottom-full duration-300"
+        >
+          <input
+            ref={searchInputRef}
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search product"
+            className="px-4 py-2 rounded-none text-black -translate-x-32 md:-translate-x-0 w-36 md:w-56"
+          />
+        </form>
+      )}
+      <Button
+        type="button"
+        className={`bg-transparent hover:bg-transparent w-auto h-auto p-0 m-0 ${showSearch ? "opacity-0" : "opacity-100"}`}
+        onClick={handleSearchClick}
+        variant="ghost"
+      >
+        <Search />
+      </Button>
+    </div>
+  );
+};
+
+export default SearchProduct;
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import React from "react";
 import { Search, ShoppingCart, CircleUser, Menu } from "lucide-react";
@@ -10,19 +11,26 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
+import SearchProduct from "../SearchProduct";
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-white w-full h-[132px] px-10">
       <div className="flex w-full justify-between ">
-        <Search className="hidden md:flex" />
-        <h1 className="text-2xl font-clash-display"><Link href="/">Avion</Link></h1>
+        <div className="hidden md:flex">
+          <SearchProduct />
+        </div>
+        <h1 className="text-2xl font-clash-display">
+          <Link href="/">Avion</Link>
+        </h1>
         <div className="flex gap-4">
           <Link href="/cart">
             <ShoppingCart className="hidden md:flex" />
           </Link>
           <CircleUser className="hidden md:flex" />
-          <Search className="md:hidden" />
+          <div className="md:hidden flex">
+            <SearchProduct />
+          </div>
           {/* Sheet below */}
           <Sheet>
             <SheetTrigger asChild>
@@ -35,15 +43,15 @@ const Header = () => {
               </SheetHeader>
               <div className="flex flex-col gap-4 mt-4">
                 <Link href="/products">All Products</Link>
-                <Link href="/products">Plant pots</Link>
-                <Link href="/products">Ceramics</Link>
-                <Link href="/products">Tables</Link>
-                <Link href="/products">Chairs</Link>
-                <Link href="/products">Crockery</Link>
-                <Link href="/products">Tableware</Link>
-                <Link href="/products">Cutlery</Link>
+                <Link href="/products">Casual Wear</Link>
+                <Link href="/products">Formal Attire</Link>
+                <Link href="/products">Active Wear</Link>
+                <Link href="/products">Accessories</Link>
+                <Link href="/products">All Products</Link>
+                <Link href="/products">Mens</Link>
+                <Link href="/products">Womens</Link>
+                <Link href="/products">Kids</Link>
                 <h2>Profile</h2>
-                <h2>Search</h2>
                 <Link href="/cart">
                   <h2>Cart</h2>
                 </Link>
@@ -55,13 +63,13 @@ const Header = () => {
       <hr className="w-full mt-4" />
       <div className="hidden md:flex items-center justify-center gap-8 mt-4 font-satoshi">
         <Link href="/products">All Products</Link>
-        <Link href="/products">Plant pots</Link>
-        <Link href="/products">Ceramics</Link>
-        <Link href="/products">Tables</Link>
-        <Link href="/products">Chairs</Link>
-        <Link href="/products">Crockery</Link>
-        <Link href="/products">Tableware</Link>
-        <Link href="/products">Cutlery</Link>
+        <Link href="/products">Casual Wear</Link>
+        <Link href="/products">Formal Attire</Link>
+        <Link href="/products">Active Wear</Link>
+        <Link href="/products">Accessories</Link>
+        <Link href="/products">Mens</Link>
+        <Link href="/products">Womens</Link>
+        <Link href="/products">Kids</Link>
       </div>
     </div>
   );
