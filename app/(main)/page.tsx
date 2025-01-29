d72e29b24345e8405842a7bcaa71d061f4b2f699
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-29 09:23:08 +0500

    Remove commented-out RecommendedProducts component from Home page for cleaner code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "@/components/Hero";
 import BestSelling from "@/components/BestSelling"
 import PopularProducts from "@/components/PopularProducts";
-// import RecommendedProducts from "@/components/RecommendedProducts";
+
 const WhatMakesUsDiff = React.lazy(() => import("@/components/WhatMakesUsDiff"));
 const JoinClub = React.lazy(() => import("@/components/JoinClub"));
 const BriefAbout = React.lazy(() => import ("@/components/BriefAbout"));
@@ -12,7 +12,6 @@ export default function Home() {
     <>
       <Hero />
       <WhatMakesUsDiff />
-      {/* <RecommendedProducts /> */}
       <BestSelling />
       <PopularProducts />
       <JoinClub />
