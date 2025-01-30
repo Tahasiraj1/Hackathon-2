5a1a638d4bdbc5d2f9163b8cef9b37c22ce7ee9f
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-30 21:50:36 +0500

    Remove unused schema types from Sanity schema definition for cleaner code

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import shipment from './shipment'
 import Products from './Products'
-import order from './order'
-import customer from './customer'
-import transaction from './transaction'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [Products, shipment, order, customer, transaction],
+  types: [Products],
 }
