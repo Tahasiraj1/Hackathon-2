1932524336a02467e4a30606d5c0f68b8558b00d
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-31 05:09:15 +0500

    Remove CategoryFilter component for codebase cleanup

--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -3,8 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { Suspense, useEffect, useState } from "react";
-import { Checkbox } from "./ui/checkbox";
-import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { ChevronDown, Heart } from "lucide-react";
 import { urlFor } from "@/sanity/lib/image";
@@ -12,18 +10,25 @@ import { Image as SanityImage } from "@sanity/types";
 import { Slider } from "./ui/slider";
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCart } from "@/lib/CartContext";
 import { WishItem } from "@/lib/CartContext";
 import { BarLoader } from "react-spinners";
-// import { Badge } from "./ui/badge";
-import { motion } from "framer-motion";
 import { containerVariants, itemVariants } from "@/lib/motion";
-import CategoryFilter from "./CategoryFilter";
+import { AnimatePresence, motion } from "framer-motion";
+import { Check } from "lucide-react";
 
+const productTypes = [
+  "Mens",
+  "Womens",
+  "Kids",
+  "Casual Wear",
+  "Formal Attire",
+  "Active Wear",
+  "Accessories",
+];
 
 interface Product {
   id: string;
@@ -38,16 +43,6 @@ interface Product {
   categories: string[];
   description: string;
 }
-
-const productTypes = [
-  "Mens",
-  "Womens",
-  "Kids",
-  "Casual Wear",
-  "Formal Attire",
-  "Active Wear",
-  "Accessories",
-];
 
 const ProductListing = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -86,6 +81,13 @@ const ProductListing = () => {
     });
   };
 
+  const toggleCategory = (category: string) => {
+    setSelectedCategories((prev) =>
+      prev.includes(category)
+        ? prev.filter((c) => c !== category)
+        : [...prev, category]
+    );
+  };
 
   const filteredProducts = selectedCategories.length
     ? products.filter((product) =>
@@ -153,7 +155,7 @@ const ProductListing = () => {
       {loading ? (
         <Suspense>
           <div className="flex justify-center items-center min-h-[400px] w-full">
-            <BarLoader color="#2A254B" /> 
+            <BarLoader color="#2A254B" />
           </div>
         </Suspense>
       ) : (
@@ -161,7 +163,94 @@ const ProductListing = () => {
           <div className="flex w-full px-2 md:px-6 lg:px-8 mt-8 gap-8">
             <div className="md:flex flex-col w-1/4 hidden">
               <h2 className="text-3xl font-clashDisplay mb-4">Product type</h2>
-              <CategoryFilter />
+              <motion.div
+                className="flex flex-wrap gap-3 overflow-visible"
+                layout
+                transition={{
+                  type: "spring",
+                  stiffness: 500,
+                  damping: 30,
+                  mass: 0.5,
+                }}
+              >
+                {productTypes.map((type) => {
+                  const isSelected = selectedCategories.includes(type);
+                  return (
+                    <motion.button
+                      key={type}
+                      onClick={() => toggleCategory(type)}
+                      layout
+                      initial={false}
+                      animate={{
+                        backgroundColor: isSelected ? "#2A254B" : "#CBD5E1",
+                      }}
+                      whileHover={{
+                        backgroundColor: isSelected ? "#363061" : "#94A3B8",
+                      }}
+                      whileTap={{
+                        backgroundColor: isSelected ? "#2A254B" : "#64748B",
+                      }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 30,
+                        mass: 0.5,
+                        backgroundColor: { duration: 0.1 },
+                      }}
+                      className={`
+                      inline-flex items-center px-4 py-2 rounded-full text-base font-medium
+                      whitespace-nowrap overflow-hidden ring-1 ring-inset
+                      ${
+                        isSelected
+                          ? "bg-[#363061] ring-[hsla(0,0%,100%,0.12)]"
+                          : "text-black ring-[hsla(0,0%,100%,0.06)]"
+                      }
+                    `}
+                    >
+                      <motion.div
+                        className="relative flex items-center"
+                        animate={{
+                          width: isSelected ? "auto" : "100%",
+                          paddingRight: isSelected ? "1.5rem" : "0",
+                        }}
+                        transition={{
+                          ease: [0.175, 0.885, 0.32, 1.275],
+                          duration: 0.3,
+                        }}
+                      >
+                        <span
+                          className={`${isSelected ? "text-white" : "text-black"}`}
+                        >
+                          {type}
+                        </span>
+                        <AnimatePresence>
+                          {isSelected && (
+                            <motion.span
+                              initial={{ scale: 0, opacity: 0 }}
+                              animate={{ scale: 1, opacity: 1 }}
+                              exit={{ scale: 0, opacity: 0 }}
+                              transition={{
+                                type: "spring",
+                                stiffness: 500,
+                                damping: 30,
+                                mass: 0.5,
+                              }}
+                              className="absolute right-0"
+                            >
+                              <div className="w-4 h-4 rounded-full bg-[#574f99] flex items-center justify-center">
+                                <Check
+                                  className="w-3 h-3 text-white"
+                                  strokeWidth={1.5}
+                                />
+                              </div>
+                            </motion.span>
+                          )}
+                        </AnimatePresence>
+                      </motion.div>
+                    </motion.button>
+                  );
+                })}
+              </motion.div>{" "}
               <h2 className="text-2xl font-clashDisplay my-4">Price</h2>
               <div className="space-y-2">
                 <Slider
@@ -188,7 +277,94 @@ const ProductListing = () => {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-72">
-                  <CategoryFilter />
+                  <motion.div
+                    className="flex flex-wrap gap-3 overflow-visible"
+                    layout
+                    transition={{
+                      type: "spring",
+                      stiffness: 500,
+                      damping: 30,
+                      mass: 0.5,
+                    }}
+                  >
+                    {productTypes.map((type) => {
+                      const isSelected = selectedCategories.includes(type);
+                      return (
+                        <motion.button
+                          key={type}
+                          onClick={() => toggleCategory(type)}
+                          layout
+                          initial={false}
+                          animate={{
+                            backgroundColor: isSelected ? "#2A254B" : "#CBD5E1",
+                          }}
+                          whileHover={{
+                            backgroundColor: isSelected ? "#363061" : "#94A3B8",
+                          }}
+                          whileTap={{
+                            backgroundColor: isSelected ? "#2A254B" : "#64748B",
+                          }}
+                          transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 30,
+                            mass: 0.5,
+                            backgroundColor: { duration: 0.1 },
+                          }}
+                          className={`
+                          inline-flex items-center px-4 py-2 rounded-full text-base font-medium
+                          whitespace-nowrap overflow-hidden ring-1 ring-inset
+                          ${
+                            isSelected
+                              ? "bg-[#363061] ring-[hsla(0,0%,100%,0.12)]"
+                              : "text-black ring-[hsla(0,0%,100%,0.06)]"
+                          }
+                        `}
+                        >
+                          <motion.div
+                            className="relative flex items-center"
+                            animate={{
+                              width: isSelected ? "auto" : "100%",
+                              paddingRight: isSelected ? "1.5rem" : "0",
+                            }}
+                            transition={{
+                              ease: [0.175, 0.885, 0.32, 1.275],
+                              duration: 0.3,
+                            }}
+                          >
+                            <span
+                              className={`${isSelected ? "text-white" : "text-black"}`}
+                            >
+                              {type}
+                            </span>
+                            <AnimatePresence>
+                              {isSelected && (
+                                <motion.span
+                                  initial={{ scale: 0, opacity: 0 }}
+                                  animate={{ scale: 1, opacity: 1 }}
+                                  exit={{ scale: 0, opacity: 0 }}
+                                  transition={{
+                                    type: "spring",
+                                    stiffness: 500,
+                                    damping: 30,
+                                    mass: 0.5,
+                                  }}
+                                  className="absolute right-0"
+                                >
+                                  <div className="w-4 h-4 rounded-full bg-[#574f99] flex items-center justify-center">
+                                    <Check
+                                      className="w-3 h-3 text-white"
+                                      strokeWidth={1.5}
+                                    />
+                                  </div>
+                                </motion.span>
+                              )}
+                            </AnimatePresence>
+                          </motion.div>
+                        </motion.button>
+                      );
+                    })}
+                  </motion.div>
                 </DropdownMenuContent>
               </DropdownMenu>
               <DropdownMenu>
@@ -223,7 +399,7 @@ const ProductListing = () => {
                     key={product.id}
                     className="block"
                   >
-                    <motion.div 
+                    <motion.div
                       className="overflow-hidden"
                       variants={containerVariants}
                       initial="hidden"
@@ -232,9 +408,9 @@ const ProductListing = () => {
                     >
                       {/* Check if image exists before rendering */}
                       {product.images?.[0] ? (
-                        <motion.div 
-                        variants={itemVariants}
-                        className="relative group"
+                        <motion.div
+                          variants={itemVariants}
+                          className="relative group"
                         >
                           <Image
                             src={urlFor(product.images[0]).url()}
@@ -274,13 +450,8 @@ const ProductListing = () => {
                           No Image Available
                         </div>
                       )}
-                      <motion.div 
-                      variants={itemVariants}
-                      className="p-4"
-                      >
-                        <h3 className="text-lg">
-                          {product.name}
-                        </h3>
+                      <motion.div variants={itemVariants} className="p-4">
+                        <h3 className="text-lg">{product.name}</h3>
                         <p className="text-gray-600">{product.price}</p>
                       </motion.div>
                     </motion.div>
