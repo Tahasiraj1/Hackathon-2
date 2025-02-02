421baa21da23a2ab0ecbee13a61de302bc148436
Tahasiraj1 <tahasiraj242@gmail.com>
2025-02-02 19:12:20 +0500

    Remove unused CardFooter import from Category component

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, CardFooter } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
