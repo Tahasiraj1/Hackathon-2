dd652ae75cb4a5f8944c948c33acdfb4f9c7a451
Tahasiraj1 <tahasiraj242@gmail.com>
2025-02-04 22:26:16 +0500

    Enhance Hero component with scroll-based animations and add useMediaQuery hook for responsive design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+"use client";
+
+import { useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 const Hero = () => {
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start start", "end start"],
+  });
+
+  const width = useTransform(scrollYProgress, [0, 0.3], ["100%", "100vw"]);
+  const scale = useTransform(scrollYProgress, [0, 0.3], [1, 1.2]);
+
+  const isLargeScreen = useMediaQuery("(min-width: 768px)");
+
   return (
-    <div className="flex items-center justify-center md:py-16 text-white font-clashDisplay">
-      <div className="flex-col px-10 w-full max-w-3xl bg-[#2A254B] h-[502px] md:h-[450px] py-20 ">
+    <motion.div
+      transition={{ duration: 1, ease: "easeInOut" }}
+      ref={containerRef}
+      style={{
+        width: isLargeScreen ? width : "100%",
+        scale: isLargeScreen ? scale : 1,
+      }}
+      className="flex items-center justify-center md:py-16 text-white font-clashDisplay relative overflow-hidden"
+    >
+      <div className="flex-col px-10 w-full max-w-3xl bg-[#2A254B] h-[502px] md:h-[450px] py-20">
         <h2 className="text-2xl">
           The furniture brand for the
           <br />
-          fututre, with timeless designs
+          future, with timeless designs
         </h2>
         <Link href="/products">
           <Button className="hidden md:flex bg-opacity-50 bg-slate-400 hover:bg-slate-400 rounded-none py-6 px-6 mt-8 mb-28">
@@ -18,7 +41,7 @@ const Hero = () => {
           </Button>
         </Link>
         <h2 className="mt-10 md:mt-0">
-          A new era in eco friendly furniture with Avelon, the French luxury
+          A new era in eco-friendly furniture with Avelon, the French luxury
           retail brand
           <br />
           with nice fonts, tasteful colors and a beautiful way to display things
@@ -39,7 +62,7 @@ const Hero = () => {
         height={400}
         className="hidden lg:flex"
       />
-    </div>
+    </motion.div>
   );
 };
 
