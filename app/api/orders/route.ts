44878d4f1c50acbddf213cca920bc71703a6b367
Tahasiraj1 <tahasiraj242@gmail.com>
2025-02-25 02:36:49 +0500

    Fix clerkId parameter in orders API route GET method from malicious users modifying the request manually and send a different clerkId to fetch someone else’s orders

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -193,7 +193,7 @@ export async function GET(request: Request) {
 
       const orders = await prisma.order.findMany({
         where: {
-          clerkId,
+          clerkId: userId,
         },
         include: {
           customerDetails: true,
