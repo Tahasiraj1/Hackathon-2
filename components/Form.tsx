818fc04c2b0561ee72ec73bc34afe4912699899c
Tahasiraj1 <tahasiraj242@gmail.com>
2025-06-18 00:36:42 +0500

    Comment out unused ShippingLabel import in Form component.

--- components/Form.tsx
+++ components/Form.tsx
@@ -27,7 +27,7 @@ import { useStripe, useElements, PaymentElement } from "@stripe/react-stripe-js"
 import { convertToSubcurrency } from '@/lib/convertToSubcurrency';
 import { formVariants, itemVariants } from '@/lib/motion';
 import { motion } from 'framer-motion';
-import { ShippingLabel } from '@/Types/types';
+// import { ShippingLabel } from '@/Types/types';
 
 const formSchema = z.object({
   firstName: z.string().min(3, "First name is required"),
